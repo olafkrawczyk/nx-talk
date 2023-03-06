1a6cbfd8315f4ab948efd377de15ac211cceb3bf
Olaf Krawczyk <olaf.r.krawczyk@gmail.com>
2023-03-06 22:22:15 +0100

    add web-app

--- apps/web-app/pages/_app.tsx
+++ apps/web-app/pages/_app.tsx
@@ -1,17 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Head>
         <title>Welcome to web-app!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
